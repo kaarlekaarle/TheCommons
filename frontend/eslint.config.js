a2831333ebfff9a694956c1101ac5a68b77e0e18
kaarlekaarle <kaarle.hurtig@gmail.com>
2025-08-22 14:07:36 +0300

    chore(lint): test overrides to reduce noise while refactoring

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -20,4 +20,12 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
+  {
+    files: ["**/__tests__/**/*", "**/*.test.ts", "**/*.test.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      "react-hooks/exhaustive-deps": "off"
+    }
+  }
 ])
